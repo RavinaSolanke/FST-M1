400b573f14459894ffe5a4fdb083ecaaf09f51c3
RavinaSolanke <153887746+RavinaSolanke@users.noreply.github.com>
2024-06-28 11:29:39 +0530

    Update Activity5.sql

--- SQL/Activities/Activity5.sql
+++ SQL/Activities/Activity5.sql
@@ -1,11 +1,8 @@
--- Update the grade score of salesmen from Rome to 200.
-UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+--Activity 5
 
--- Update the grade score of James Hoog to 300.
-UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+UPDATE salesman SET grade= 200 WHERE salesman_city='Rome';
 
--- Update the name McLyon to Pierre.
-UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+UPDATE salesman SET grade= 300 WHERE salesman_name='James Hoog';
 
--- Display modified data
-SELECT * FROM salesman;
+UPDATE salesman SET salesman_name= 'Pierre' WHERE salesman_name='McLyone';
+SELECT * FROM Salesman;
